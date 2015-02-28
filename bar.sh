621eb41ad07d5226a8756f0bb1baa9695eaed00d
tatosaurus <tato@uribe.com.mx>
2015-02-28 12:36:08 -0600

    first commit

---
+++ bar.sh
@@ -0,0 +1,2 @@
+## Make changes to the size of the bar depending on the resolution of your display
+./var/bar_mk.sh | bar -p -g 1920x26+0+0 -f -*-profont-*-*-*-15-*-*-*-*-*-* -B#FF151515 -F#FFDADADA
