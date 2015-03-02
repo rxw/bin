74342d8c7d6a4a4803f84f5081e0441a24921365
tatosaurus <tato@uribe.com.mx>
2015-03-01 21:16:16 -0600

    fixed bar and run

--- bar.sh
+++ bar.sh
@@ -1,6 +1,2 @@
 ## Make changes to the size of the bar depending on the resolution of your display
-<<<<<<< HEAD
 ./var/bar_mk.sh | bar -p -g 1366x15+0+0 -f -*-lemon-*-*-*-10-*-*-*-*-*-* -B#FF151515 -F#FFDADADA
-=======
-./var/bar_mk.sh | bar -p -g 1920x26+0+0 -f -*-profont-*-*-*-15-*-*-*-*-*-* -B#FF151515 -F#FFDADADA
->>>>>>> 2a8d09edc84ff6a8fefe3280ed945129018fcfdc
