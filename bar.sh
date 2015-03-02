a243e1f6a021f2489af6bff49580401f7b343d55
tatosaurus <tato@uribe.com.mx>
2015-03-01 21:05:00 -0600

    added chromebook bin

---
+++ bar.sh
@@ -0,0 +1,2 @@
+## Make changes to the size of the bar depending on the resolution of your display
+./var/bar_mk.sh | bar -p -g 1366x15+0+0 -f -*-lemon-*-*-*-10-*-*-*-*-*-* -B#FF151515 -F#FFDADADA
---
+++ runprompt.sh
@@ -0,0 +1,1 @@
+dmenu_run -nb rgb:15/15/15 -nf rgb:DA/DA/DA -sb rgb:AE/FF/00 -sf rgb:15/15/15 -l 10 -fn -*-lemon-*-*-*-10-*-*-*-*-*-*
