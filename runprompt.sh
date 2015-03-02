c75c3ee49f9b4d1e133231f67471f83b048ace2c
tatosaurus <tato@uribe.com.mx>
2015-03-01 21:10:43 -0600

    added bar and run

---
+++ runprompt.sh
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+dmenu_run -nb rgb:15/15/15 -nf rgb:DA/DA/DA -sb rgb:AE/FF/00 -sf rgb:15/15/15 -l 10 -fn -*-lemon-*-*-*-10-*-*-*-*-*-*
+=======
+dmenu_run -nb rgb:15/15/15 -nf rgb:DA/DA/DA -sb rgb:AE/FF/00 -sf rgb:15/15/15 -l 10 -fn -*-gohufont-medium-*-*-14-*-*-*-*-iso10646-*
+>>>>>>> 2a8d09edc84ff6a8fefe3280ed945129018fcfdc
