9bda5e934363b44620508277e50991dd6822f058
tato <tato@uribe.com.mx>
2015-04-17 22:36:15 -0500

    deleted mountem

--- mountem.sh
+++
@@ -1,3 +0,0 @@
-echo pa55w0rd3 | sudo -S mount -t nfs 192.168.1.10:/music /home/tato/usr/music
-echo pa55w0rd3 | sudo -S mount -t nfs 192.168.1.10:/media /home/tato/usr/anime
-echo pa55w0rd3 | sudo -S mount -t nfs 192.168.1.10:/ebooks /home/tato/usr/books
