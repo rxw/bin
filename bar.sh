79fe3d87350c6dca53eb22ce02cfefcb6a85f4bd
tatosaurus <tato@uribe.com.mx>
2015-04-20 19:46:06 -0500

    changed bar

--- bar.sh
+++ bar.sh
@@ -1,9 +1,9 @@
-FONT="Consolas-11"
-FALLBACK="Icons-11"
+FONT="-*-lemon-*-*-*-*-10-*-*-*-*-*-*-*"
+FALLBACK="-*-stlarch-*-*-*-*-10-*-*-*-*-*-*-*"
 BGCOLOR="#FF37393d"
 FGCOLOR="#FFfafafa"
 
-./bin/bar_mk.sh | lemonbar -p -g 1520x32+200+0 -u 3 -f ${FONT},${FALLBACK} -B${BGCOLOR} -F${FGCOLOR} | while read -r line; do
+./bin/bar_mk.sh | bar -p -g 1520x32+200+0 -u 3 -f ${FONT},${FALLBACK} -B${BGCOLOR} -F${FGCOLOR} | while read -r line; do
   $line
 done
 
