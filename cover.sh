7a0d6a7bc518b9f62573d9392874823c860c1451
tato <tato@uribe.com.mx>
2015-06-30 20:35:10 -0500

    Made feh the bg manageer

--- cover.sh
+++ cover.sh
@@ -3,15 +3,9 @@ path=$(mpc -f "%file%" | head -n 1 | cut -f 1-3 -d "/")
 
 cover() {
   if [[ "$path" != "" ]]; then
-<<<<<<< HEAD
     convert "$music_path/$path/cover.jpg" -scale 400x400 \( +clone -background black -shadow 60x5+10+10 \) +swap -background transparent -layers merge +repage /tmp/art.png
      convert $HOME/usr/pictures/bg.png /tmp/art.png -geometry +1480+50 -composite /tmp/bg.png
     feh --bg-fill /tmp/bg.png
-=======
-    convert "$music_path/$path/cover.jpg" -scale 250x250 \( +clone -background black -shadow 60x5+10+10 \) +swap -background transparent -layers merge +repage /tmp/art.png
-     convert $HOME/usr/img/bg.png /tmp/art.png -geometry +3480+50 -composite /tmp/bg.png
-    nitrogen --set-auto /tmp/bg.png
->>>>>>> 5fcad8631dc28222ea4afdd4b624b35531d9519e
     #feh --bg-tile "$music_path/$path/cover.jpg"
   fi
 }
@@ -25,10 +19,6 @@ while true; do
     path=$(mpc -f "%file%" | head -n 1 | cut -f 1-3 -d "/")
     pathnew="$path"
 
-<<<<<<< HEAD
-    pkill -f "feh --title music_art"
-=======
->>>>>>> 5fcad8631dc28222ea4afdd4b624b35531d9519e
     cover
   fi
 
