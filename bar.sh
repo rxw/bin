47885b40035327e2f53827ef0fc26db0ed43a890
tato <tato@uribe.com.mx>
2015-07-02 21:44:46 -0500

    Added /bin/bash to bar.sh.... sorry

--- bar.sh
+++ bar.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 FONT='-*-gohufont-medium-*-*-*-11-*-*-*-*-*-*-*'
 FALLBACK='-*-stlarch-*-*-*-*-10-*-*-*-*-*-*-*'
-BGCOLOR="#FF1f1f1f"
-FGCOLOR="#FFcacaca"
 
-./bin/bar_mk.sh | bar -p -g 1520x35+200+0 -u 3 \
--f ${FONT},${FALLBACK} \
--B${BGCOLOR} -F${FGCOLOR} | while read -r line; do
+./bin/bar_mk.sh | lemonbar -p -g 1520x35+200+0 -u 3 \
+-f 'Monaco for Powerline-11',${FALLBACK} \
+-B${background} -F${foreground} | while read -r line; do
   $line
 done
 
