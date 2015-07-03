5dd42c2686fd7705a722758db9cbc662110e0d21
tato <tato@uribe.com.mx>
2015-07-02 21:20:45 -0500

    Added jpg to png conversion to bgcolors

--- bgcolors.sh
+++ bgcolors.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-#if [ $(tail -c 3 $1)="png" ]
-#then
-#  echo "Not a png."
-#  exit
-#fi
+folder=$(dirname $1)
+name=${1::-4}
+image=$1
+if [ $(echo $1 | tail -c 4)="png" ]
+then
+  image="${name}.png"
+  convert $1 $image
+fi
 
 mv /home/$USER/.Xdefaults /home/$USER/.Xdefaults.bak
 head -n 9 .Xdefaults.bak > .Xdefaults
 
 cont=0
-for i in $(colors -en 16 $1)
+for i in $(colors -en 16 $image)
 do
   cont=$(( cont + 1 ))
   case $cont in
@@ -27,7 +30,6 @@ done
 printf "\n*background:%s" $background >> /home/$USER/.Xdefaults
 printf "\n*foreground:%s" $foreground >> /home/$USER/.Xdefaults
 
-folder=$(dirname $1)
-feh --bg-fill $1
+feh --bg-fill $image
 
-cp $1 "$folder/wp.png"
+cp $image "$folder/wp.png"
